339e7a585dae0fa34bc3077e4e4d3055d64dd069
Aman Patel <106889345+Amanpatel2529@users.noreply.github.com>
2022-10-05 16:25:23 +0530

    Add files via upload
    WAP to calculate area of a circle using functions.
    WAP to swap two values using functions.
    WAP to convert time to minutes using functions.
    ( input 3 variables namely hours , minutes and seconds
    Convert everything into minutes.)
    WAP to sum the series up to n (Input n) 
       +  +  +……………………………………..

---
+++ Lab5/Lab5-source2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>        //C++ libriry which is calculating "pow" and PI=3.14
+
+using namespace std;
+
+//Program to swap two values using functions
+
+int main2()
+{
+	int a, b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	swap(a, b);                            //C++ function to swap two numbers 
+	cout << a << " & " << b << endl;       //swap is the function which is contained in c++ ==>
+	                                       //==> there is no need to open new function to swap
+	return 0;
+}                                          //ending the function main
